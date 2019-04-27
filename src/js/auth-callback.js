dd85c8f69ef7e981b687d6f367689a988c1e1c9e
jeff <jeffreestyle@163.com>
2019-04-27 16:37:23 +0800

    淘宝授权接口

--- src/js/auth-callback.js
+++ src/js/auth-callback.js
@@ -2,19 +2,21 @@
 * 授权回调页面
 * */
 var AuthCallback = {
+    uid: null, //uid
     extTime: null, //用户session过期时间
     session: null, //用户session
     state: null, //标志位（实际是细银用户id）
     userId: null, //淘宝id
     userNick: null, //淘宝昵称
     init: function () {
-        this.extTime = Common.getUrlParam('ext_time');
-        this.session = Common.getUrlParam('session');
+        this.uid = Common.getUrlParam('taobao_open_uid');
+        this.extTime = Common.getUrlParam('re_expires_in');
+        this.session = Common.getUrlParam('access_token');
         this.state = Common.getUrlParam('state');
-        this.userId = Common.getUrlParam('user_id');
-        this.userNick = Common.getUrlParam('user_nick');
+        this.userId = Common.getUrlParam('taobao_user_id');
+        this.userNick = Common.getUrlParam('taobao_user_nick');
 
-        if (!this.session || !this.extTime) {
+        if (!this.uid || !this.session || !this.extTime) {
             Common.showErrorPage("授权失败，请退出重试");
             return;
         }
@@ -25,6 +27,7 @@ var AuthCallback = {
     registryTaobao: function() {
         var context = this,
             data = {
+                uid: this.uid,
                 extTime: this.extTime,
                 session: this.session,
                 state: this.state,
